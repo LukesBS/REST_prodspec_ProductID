c2d4f0bfd08968ae9f6ae001dbe565fa233e63fd
Luke <lukas.vetter@stud.bs7-augsburg.de>
2023-03-14 19:37:47 +0100

    removed comments in db.js

--- models/db.js
+++ models/db.js
@@ -1,7 +1,6 @@
 const mysql = require("mysql");
 const dbConfig = require("../config/dbconfig.js");
 
-// Erzeugen der Datenbankverbindung
 const dbConfigPath = {
   host: dbConfig.HOST,
   port: dbConfig.PORT,
@@ -12,30 +11,24 @@ const dbConfigPath = {
 
 var connection;
 function handleDisconnect() {
-  connection = mysql.createConnection(dbConfigPath); // Recreate the connection, since the old one cannot be reused.
+  connection = mysql.createConnection(dbConfigPath);
   connection.connect(function onConnect(err) {
-    // The server is either down
     if (err) {
-      // or restarting (takes a while sometimes).
       console.log("error when connecting to db:", err);
-      setTimeout(handleDisconnect, 10000); // We introduce a delay before attempting to reconnect,
-    } // to avoid a hot loop, and to allow our node script to
-  }); // process asynchronous requests in the meantime.
-  // If you're also serving http, display a 503 error.
+      setTimeout(handleDisconnect, 10000);
+    }
+  });
   connection.on("error", function onError(err) {
     console.log("db error", err);
     if (err.code == "PROTOCOL_CONNECTION_LOST") {
-      // Connection to the MySQL server is usually
-      handleDisconnect(); // lost due to either server restart, or a
+      handleDisconnect();
     } else if (err.code == "ECONNRESET") {
       handleDisconnect();
     } else {
-      // connnection idle timeout (the wait_timeout
-      throw err; // server variable configures this)
+      throw err;
     }
   });
 }
 handleDisconnect();
 
-// Export der offenen Datenbanverbindung
 module.exports = connection;
